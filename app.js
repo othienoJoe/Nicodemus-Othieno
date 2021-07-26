7ddfe7d278bc9d1e759c13f309fb16ad67c70c33
othieno <nicothieno@gmail.com>
2021-07-26 21:07:36 +0300

    added running time

--- app.js
+++ app.js
@@ -1,4 +1,32 @@
-<script>
+// Heading time interval
+window.onload=function(){getTime();}  
+function getTime(){  
+  var today=new Date();  
+  var h=today.getHours();  
+  var m=today.getMinutes();  
+  var s=today.getSeconds();
+  function getDate(){
+    var date=new Date();
+    var day=date.getDate();  
+    var month=date.getMonth()+1;  
+    var year=date.getFullYear();  
+document.write("<br>Date is: "+day+"/"+month+"/"+year); 
+}
+// add a zero in front of numbers<10  
+m=checkTime(m);  
+s=checkTime(s);  
+
+document.getElementById('text').innerHTML=h+":"+m+":"+s;  
+setTimeout(function(){getTime()},1000);  
+} 
+
+setInterval("getTime()",1000);//another way  
+function checkTime(i){  
+if (i<10){  
+  i="0" + i;  
+ }  
+return i;  
+} 
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   var firebaseConfig = {
@@ -13,7 +41,7 @@
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   firebase.analytics();
-</script>
+
 
 var database=firebase.database();
 
